39250397d5b51d475bab7720936b4701493415af
JagdevMishra <50991410+JagdevMishra@users.noreply.github.com>
2023-01-31 13:40:26 +0530

    Cognito user login page

---
+++ AWS_Web_Authentication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AWS_Web_Authentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AWS_Web_Authentication/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(options =>
+{
+    options.ResponseType = builder.Configuration["Authentication:Cognito:ResponseType"];
+    // MetadataAddress Format: "https: //cognito-idp.{your-region-id}.amazonaws.com/{your-user-pool-id}/.well-known/openid-configuration"
+    options.MetadataAddress = builder.Configuration["Authentication:Cognito:MetadataAddress"];
+    options.ClientId = builder.Configuration["Authentication:Cognito:ClientId"];
+    options.ClientSecret = builder.Configuration["Authentication:Cognito:ClientSecret"];
+    options.SaveTokens = true;
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("email");
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        NameClaimType = "cognito:username",
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment()) 
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
